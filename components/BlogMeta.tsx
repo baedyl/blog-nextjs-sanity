9d2d1ec09669572ffdc25f265d060f43eeee2c48
Emmanuel <eba@safetrade101.com>
2022-12-26 02:48:21 +0100

    Add google ad sense code in blog meta

--- components/BlogMeta.tsx
+++ components/BlogMeta.tsx
@@ -5,6 +5,8 @@
 export default function BlogMeta() {
   return (
     <>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8055354502682017"
+        crossorigin="anonymous"></script>
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
       <link
         rel="apple-touch-icon"
